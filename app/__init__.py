6f6d2cfeba1f7c1f608f916fae76b7ae6f5d6141
Jeffrey Lam <jeffrey.lam@sjsu.edu>
2024-04-20 19:06:04 -0700

    Register and login page frontend implemented

--- app/__init__.py
+++ app/__init__.py
@@ -3,4 +3,9 @@
 from flask import Flask
 myapp_obj = Flask(__name__)
 
-from app import routes
+from app import routes  #The routes get imported below the Flask() object call
+
+#Required when using forms
+myapp_obj.config.from_mapping(  
+    SECRET_KEY = 'blahblahblah',
+)
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,30 @@
 from app import myapp_obj
 from flask import render_template
+from forms import RegistrationForm
+from forms import LoginForm
 
 @myapp_obj.route("/")
 def index():
     return render_template('index.html')
+
+@myapp_obj.route("/login", methods=['GET','POST'])
+def login():    
+    form = LoginForm()
+    if form.validate_on_submit():  #If Login Form submit is pressed
+        return "<p>10101</p>"
+        #Check if email exists against db
+        #If exists                                                                   
+            #Check password
+                #If PW GOOD LOGIN (AUTHENTICATE)
+                #ELSE RETRY
+    return render_template('login.html', form=form)
+
+@myapp_obj.route("/register", methods=['GET','POST'])
+def register():    
+    form = RegistrationForm()
+    if form.validate_on_submit():  #If Login Form submit is pressed
+        return "<p>10101</p>"
+        #Check if email exists against db
+        #If exists send msg
+        #Else continue                                                             
+    return render_template('register.html', form=form)
